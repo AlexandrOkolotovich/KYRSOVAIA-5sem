2125d93e9ff000927cada4cbebf9069b09147ec5
Alexandr Okolotovich <okolotovich2000@mail.ru>
2020-12-02 01:39:05 +0300

    add buttons commit

--- client/src/by/bsuir/coursework/controllers/ControllerAdmin.java
+++ client/src/by/bsuir/coursework/controllers/ControllerAdmin.java
@@ -3,9 +3,13 @@ package by.bsuir.coursework.controllers;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -21,6 +25,88 @@ public class ControllerAdmin {
 
     @FXML
     private Label dateTime;
+
+    @FXML
+    private Button userButton;
+
+    @FXML
+    private Button listUsersButton;
+
+    @FXML
+    private Button addMovieButton;
+
+    @FXML
+    private Button listMovieButton;
+
+    @FXML
+    private Button makeScheduleButton;
+
+    @FXML
+    private Button viewScheduleButton;
+
+    @FXML
+    private Button estimateLossesButton;
+
+    @FXML
+    private Button cinemaInfoButton;
+
+    @FXML
+    private Pane listUsersPane;
+
+    @FXML
+    private Pane viewSchedulePane;
+
+    @FXML
+    private Pane placesCinemaPane;
+
+    @FXML
+    private Pane addMoviePane;
+
+    @FXML
+    private Pane listMoviePane;
+
+    @FXML
+    private Pane makeSchedulePane;
+
+    @FXML
+    private ScrollPane cinemaInfoPane;
+
+    @FXML
+    private Pane userPane;
+
+    @FXML
+    private Pane estimateLossesPane;
+
+    public ControllerAdmin() {
+    }
+
+    @FXML
+    void handleCliks(ActionEvent event) {
+        if(event.getSource()==userButton){
+            userPane.toFront();
+        }
+        else if(event.getSource()==listUsersButton){
+            listUsersPane.toFront();
+        }
+        else if(event.getSource()==addMovieButton){
+            addMoviePane.toFront();
+        }
+        else if(event.getSource()==listMovieButton){
+            listMoviePane.toFront();
+        }
+        else if(event.getSource()==makeScheduleButton){
+            makeSchedulePane.toFront();
+        }
+        else if(event.getSource()==viewScheduleButton){
+            viewSchedulePane.toFront();
+        }
+        else if(event.getSource()==estimateLossesButton){
+            estimateLossesPane.toFront();
+        }
+        else if(event.getSource()==cinemaInfoButton){
+            cinemaInfoPane.toFront();
+        }
+    }
 
     @FXML
     void initialize() {
